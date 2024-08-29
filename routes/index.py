e82e83b4099bfabf441cb921b1966470d1a19ee1
Miniki7 <pasinimirela@gmail.com>
2024-08-29 16:08:28 -0300

    index certo

--- routes/index.py
+++ routes/index.py
@@ -1,8 +1,11 @@
 from routes.turmaRoutes import turma
 from routes.fotosRoutes import fotos
+from routes.atividadesRoutes import atividades
+from routes.auxiliarRoutes import auxiliar
 
 
 def default_routes(app):
     turma(app)
     fotos(app)
-    
+    atividades(app)
+    auxiliar(app)
