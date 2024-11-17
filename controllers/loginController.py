865f1b37d829b269961b558473253fbdaa2195ac
PaolaBrina <paolabrina@icloud.com>
2024-11-17 10:27:39 -0300

    loginController

--- controllers/loginController.py
+++ controllers/loginController.py
@@ -14,32 +14,36 @@ def loginController():
 
     tipos = []
     nomes = []
+    codigos = []
 
     if responsavel_existe:
         tipos.append('responsavel')
+        codigos.append(responsavel_existe.codigo)
         if len(nomes) < 1:
             nomes.append(responsavel_existe.nome)
             print(f"Responsável encontrado: {responsavel_existe.nome}")
     
     if professor_existe:
         tipos.append('professor')
+        codigos.append(professor_existe.codigo)
         if len(nomes) < 1:
             nomes.append(professor_existe.nome)  
             print(f"Professor encontrado: {professor_existe.nome}")
     
     if colaborador_existe:
         tipos.append('colaborador')
+        codigos.append(colaborador_existe.codigo)
         if len(nomes) < 1:
             nomes.append(colaborador_existe.nome)
             print(f"Colaborador encontrado: {colaborador_existe.nome}")
 
     if len(tipos) > 1:
-        return jsonify({'status': 'multi', 'data': tipos, 'nomes': nomes})
+        return jsonify({'codigo': codigos, 'status': 'multi', 'data': tipos, 'nomes': nomes})
     elif 'responsavel' in tipos:
-        return jsonify({'status': 'responsavel', 'nome': responsavel_existe.nome})
+        return jsonify({'codigo': codigos, 'status': 'responsavel', 'nome': nomes})
     elif 'professor' in tipos:
-        return jsonify({'status': 'professor', 'nome': professor_existe.nome})
+        return jsonify({'codigo': codigos, 'status': 'professor', 'nome': nomes})
     elif 'colaborador' in tipos:
-        return jsonify({'status': 'colaborador', 'nome': colaborador_existe.nome})
+        return jsonify({'codigo': codigos, 'status': 'colaborador', 'nome': nomes})
     else:
         return jsonify({'status': 'nao_encontrado'})
